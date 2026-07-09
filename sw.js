2ae1cd8f133d6d3436b74d176c2be80fd8dd1c75
jingeun-git <asd1684@gmail.com>
2026-07-09 13:28:05 +0900

    Update cache version from to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taskboard-shell-v4';
+const CACHE = 'taskboard-shell-v5';
 const SHELL = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
